0d59135ac05cfb33e69899864f2b90319dd3ace5
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-05-22 22:20:06 +0200

    Added lodash

--- public/game/js/level.js
+++ public/game/js/level.js
@@ -1,21 +1,22 @@
 'use strict';
 
 define([
+    '../../lib/qrcode'
 ], function () {
 
-	var game;
-	var code;
+    var game;
+    var code;
 
-	var Level = function (g) {
-		game = g;
-	};
+    var Level = function (g) {
+        game = g;
+    };
 
-	Level.prototype.load = function (levelNumber) {
-		/*code = new QRCode();
-		var grid = code.makeCode();
-		console.log(grid);*/
-	};
+    Level.prototype.load = function (levelNumber) {
+        /*code = new QRCode();
+        var grid = code.makeCode();
+        console.log(grid);*/
+    };
 
-	return Level;
+    return Level;
 
 });
