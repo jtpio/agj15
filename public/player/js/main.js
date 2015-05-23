dbb483acc3749f63976c3bb8cc44b56d93053e13
Husenap <Hussein_t_97@hotmail.com>
2015-05-23 23:42:32 +0200

    audio + spritesheet + phone controls
    Conflicts:
    	public/game/js/playerManager.js

--- public/player/js/main.js
+++ public/player/js/main.js
@@ -15,6 +15,14 @@ requirejs([
     var btn1 = null;
     var btn2 = null;
     var btn3 = null;
+    var bmd = null;
+
+    var check = null, cross = null;
+
+    var btnLocations = {
+        x:[0.25, 0.75, 0.25, 0.75],
+        y:[0.25, 0.25, 0.75, 0.75]
+    };
 
     var btnScale = Math.min(windowWidth / 2, windowHeight / 2) / btnHeight;
 
@@ -26,106 +34,110 @@ requirejs([
     }, false, false);
 
 
-
-
     var networkManager = new NetworkManager(game);
     networkManager.setupClient();
 
     function preload () {
         // Phaser.Canvas.setSmoothingEnabled(game.context, false);
         game.load.atlasJSONHash('controls', '../assets/img/game_spritesheet.png', '../assets/img/game_spritesheet.json');
+        game.load.image('check', '../assets/img/check.png');
+        game.load.image('cross', '../assets/img/cross.png');
     }
 
     function create () {
         game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
         game.stage.backgroundColor = '#808080';
 
+        createIcon(check, 'check');
+        createIcon(cross, 'cross');
 
-        btn0 = game.add.button(
-            windowWidth * 0.25, 
-            windowHeight * 0.25, 
-            'controls', 
-            function(){
-                console.log('Moving to 0');
-                networkManager.getClient().sendCommand('goto', {
-                    glyph: 0
-                });
-            }, 
-            this, 
-            'Phone_Glyph001.PNG', 
-            'Phone_Glyph001.PNG', 
-            'Phone_Glyph001.PNG'
-        );
-        btn0.name = 'btn0';
-
-
-        btn1 = game.add.button(
-            windowWidth * 0.75, 
-            windowHeight * 0.25, 
-            'controls', 
-            function(){
-                console.log('Moving to 1');
-                networkManager.getClient().sendCommand('goto', {
-                    glyph: 1
-                });
-            }, 
-            this, 
-            'Phone_Glyph002.png', 
-            'Phone_Glyph002.png', 
-            'Phone_Glyph002.png'
-        );
-        btn1.name = 'btn1';
-
-
-        btn2 = game.add.button(
-            windowWidth * 0.25, 
-            windowHeight * 0.75, 
-            'controls', 
-            function(){
-                console.log('Moving to 2');
-                networkManager.getClient().sendCommand('goto', {
-                    glyph: 2
-                });
-            }, 
-            this, 
-            'Phone_Glyph003.PNG', 
-            'Phone_Glyph003.PNG', 
-            'Phone_Glyph003.PNG'
-        );
-        btn2.name = 'btn2';
-
-
-        btn3 = game.add.button(
-            windowWidth * 0.75, 
-            windowHeight * 0.75, 
-            'controls', 
-            function(){
-                console.log('Moving to 3');
-                networkManager.getClient().sendCommand('goto', {
-                    glyph: 3
-                });
-            }, 
-            this, 
-            'Phone_Glyph004.PNG', 
-            'Phone_Glyph004.PNG', 
-            'Phone_Glyph004.PNG'
-        );
-        btn3.name = 'btn3';
-
+        btn0 = createButton(0);
+        btn1 = createButton(1);
+        btn2 = createButton(2);
+        btn3 = createButton(3);
 
         // Drawing lines
-        var bmd = game.add.bitmapData(windowWidth, windowHeight);
-        var color = 'white';
-        
+        bmd = game.add.bitmapData(windowWidth, windowHeight);
         bmd.ctx.beginPath();
         bmd.ctx.lineWidth = "4";
-        bmd.ctx.strokeStyle = color;
+        bmd.ctx.strokeStyle = 'white';
         bmd.ctx.stroke();
 
-        var sprite = game.add.sprite(0, 0, bmd);
+        game.add.sprite(0, 0, bmd);
 
-        bmd.clear();
+        drawCross();
+
+        networkManager.getClient().addEventListener('puzzle', function(data){
+            console.log(data);
+            var scale = (windowHeight/4)/btnHeight;
+            var offsetY = (windowHeight/4);
+
+            btnToOption(btn0, 0, data);
+            btnToOption(btn1, 1, data);
+            btnToOption(btn2, 2, data);
+            btnToOption(btn3, 3, data);
+           
+            bmd.clear();
+        });
+    }
+    function createIcon(sprite, name){
+        sprite = game.add.sprite(0, 0, name);
+        sprite.scale.setTo(btnScale);
+        sprite.anchor.setTo(0.5);
+        sprite.position.setTo(windowWidth/2, sprite.height);
+    }
+    function createButton(index){
+        var btn = game.add.button(
+            windowWidth * btnLocations.x[index], 
+            windowHeight * btnLocations.y[index], 
+            'controls', 
+            function(){
+                console.log('Moving to '+index);
+                networkManager.getClient().sendCommand('goto', {
+                    glyph: index
+                });
+                resetButtons();
+            },
+            this, 
+            'Phone_Glyph00'+(index+1),
+            'Phone_Glyph00'+(index+1),
+            'Phone_Glyph00'+(index+1)
+        );
+        btn.name = 'btn'+index;
+        btn.anchor.setTo(0.5);
+        btn.scale.setTo(btnScale);
+        return btn;
+    }
+    function btnToOption(btn, index, data){
+        var scale = (windowHeight/4)/(btnHeight*2);
+        var offsetY = (windowHeight/4);
+        game.add.tween(btn.scale).to({x:scale,y:scale}, 250, Phaser.Easing.Quadratic.InOut, true).onComplete.add(function(){
+            game.add.tween(btn).to({x:btn.width/2,y:(index+1)*offsetY-offsetY/2}, 250, Phaser.Easing.Quadratic.InOut, true).onComplete.add(function(){
+                var text = game.make.text(0, 0, data[index], { font: "bold "+btn.height/2+"px Arial", fill: "#fff" });
+                text.anchor.set(0, 0.5);
+                bmd.draw(text, btn.x+btn.width/2, btn.y);
+            },this);
+        }, this);
+    }
+    function optionToBtn(btn, index){
+        game.add.tween(btn.scale).to({x:btnScale,y:btnScale}, 250, Phaser.Easing.Quadratic.InOut, true);
+        game.add.tween(btn).to(
+            {   
+                x:windowWidth*btnLocations.x[index],
+                y:windowHeight*btnLocations.y[index]
+            }, 
+            250, Phaser.Easing.Quadratic.InOut, true);
         
+    }
+    function resetButtons(){
+        optionToBtn(btn0, 0);
+        optionToBtn(btn1, 1);
+        optionToBtn(btn2, 2);
+        optionToBtn(btn3, 3);
+        drawCross();
+    }
+    function drawCross(){
+        bmd.clear();
         bmd.ctx.beginPath();
         bmd.ctx.moveTo(windowWidth / 2, 0);
         bmd.ctx.lineTo(windowWidth / 2, windowHeight);
@@ -142,19 +154,12 @@ requirejs([
         bmd.ctx.closePath();
         bmd.render();
         
-
-        setupUI();
     }
-
-    function restart () {
-    }
-
     function update () {
 
     }
-
     function render () {
-        // debug stuff maybe
+        
     }
 
     function toggleFullScreen() {
@@ -164,21 +169,4 @@ requirejs([
             game.scale.startFullScreen();
         }
     }
-
-
-
-    var setupUI = function(){
-        // Init buttons
-        btn0.anchor.setTo(0.5, 0.5);
-        btn0.scale.setTo(btnScale);
-        
-        btn1.anchor.setTo(0.5, 0.5);
-        btn1.scale.setTo(btnScale);
-        
-        btn2.anchor.setTo(0.5, 0.5);
-        btn2.scale.setTo(btnScale);
-
-        btn3.anchor.setTo(0.5, 0.5);
-        btn3.scale.setTo(btnScale);
-    };
 });
