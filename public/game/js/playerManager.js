73f08a78d15f047f36fa9cddbd944211444001b1
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-05-23 16:14:41 +0200

    send fake puzzle to player

--- public/game/js/playerManager.js
+++ public/game/js/playerManager.js
@@ -3,16 +3,23 @@
 define([], function () {
 
     var game;
+    var players = {};
 
     var PlayerManager = function (g) {
         game = g;
     };
 
     PlayerManager.prototype.add = function(netPlayer) {
+
         console.log('add player', netPlayer);
+
+        players[netPlayer.id] = netPlayer;
+
         netPlayer.addEventListener('goto', function(data){
         	console.log(data);
         });
+
+        netPlayer.sendCommand('puzzle', ['a','b','c']);
     };
 
     return PlayerManager;
