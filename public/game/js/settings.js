4fa66c0125565f33a0d6dc9dd3d363b0f6b1a61a
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-05-24 03:46:50 +0200

    10s transition

--- public/game/js/settings.js
+++ public/game/js/settings.js
@@ -10,7 +10,7 @@ define(function () {
 	    DIFFICULTY_COLORS: [0x00ff99, 0xffff00, 0xff6666],
 		GLYPHS_IDS: [0, 1, 2, 3],
 		GLYPHS_NAMES: ['', '', '', ''],
-		TRANSITION_TIME: 1000 * 4,
+		TRANSITION_TIME: 1000 * 10,
 		PLAYING_TIME: 1000 * 45
 	};
 
